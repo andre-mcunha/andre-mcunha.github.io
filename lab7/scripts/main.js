6f7d085b3d43749f50815f6bccefa7abb044dfd3
André Cunha <151823216+andre-mcunha@users.noreply.github.com>
2024-10-31 12:43:17 +0000

    lab7 unfinished

---
+++ lab7/scripts/main.js
@@ -0,0 +1,61 @@
+//1.Passa por aqui!---------------------------------------------------------
+const item = document.getElementById("1")
+
+item.addEventListener('mouseover', passar);
+item.addEventListener('mouseout', sair);
+
+function passar() {
+    item.textContent = "Obrigado por passar!"
+}
+
+function sair() {
+    item.textContent = "Passa por aqui!"
+}
+
+//2.Pinta-me!----------------------------------------------------------------
+const red = document.getElementById("red")
+const blue = document.getElementById("blue")
+const green = document.getElementById("green")
+const item2 = document.getElementById("2")
+
+red.addEventListener('click', pintar);
+green.addEventListener('click', pintar);
+blue.addEventListener('click', pintar);
+
+
+function pintar() {
+    if (event.target.id === "red") {
+        item2.style.color = "red";
+    } else if (event.target.id === "blue") {
+        item2.style.color = "blue";
+    } else if (event.target.id === "green") {
+        item2.style.color = "green";
+    }
+}
+
+
+//3.Experimenta escrever...---------------------------------------------------
+
+
+
+//4.Escolhe uma cor em ingles:
+const botaoSubmeter = document.getElementById("submeter")
+const corFundo = document.getElementById("corFundo")
+
+botaoSubmeter.addEventListener('click', mudaCorFundo);
+
+function mudaCorFundo () {
+
+    document.body.style.backgroundColor = corFundo.value;
+}
+
+//5.Conta---------------------------------------------------------------------
+const botaoConta = document.getElementById("5")
+const counter = document.getElementById("counter")
+let contagem = 0
+
+botaoConta.addEventListener('click', contar);
+
+function contar () {
+    counter.textContent = ++contagem;
+}
